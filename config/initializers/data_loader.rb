c4a7885bf8380e1c867db17c3ec2ceaebb659d32
Mark Vance <vance.139@osu.edu>
2011-10-23 17:59:08 -0400

    Thread in data_loader.rb

--- config/initializers/data_loader.rb
+++ config/initializers/data_loader.rb
@@ -1,6 +1,9 @@
-puts "data_loader.rd sleeping"
-sleep(10)
-puts "data_loader.rb Starting"
+t = Thread.new do
 
-load "#{RAILS_ROOT}/Rakefile"
-Rake::Task['bootstrap:all'].invoke
+  puts "data_loader.rd sleeping"
+  sleep(10)
+  puts "data_loader.rb Starting"
+
+  load "#{RAILS_ROOT}/Rakefile"
+  Rake::Task['bootstrap:all'].invoke
+end
