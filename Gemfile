ab02d55b9ba6d7df85524a1e9f9b58bd27f6648c
Mark Vance <vance.139@osu.edu>
2011-10-28 14:39:42 -0500

    Changed database to PostgreSQL to test with CloudFoundry and Heroku

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.9'
 
 gem 'sqlite3'
 gem 'devise'
+gem 'pg'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,23 @@ class ApplicationController < ActionController::Base
 
   helper_method :league_admin?
   helper_method :has_profile?
-  
+
   private
-  
+
   def league_admin?
     if league_admin
       true
     else
-      flash
+      
     end
   end
-  
+
   def league_admin
     if has_profile?
       current_user.profile.league.admin.eql?(current_user.id)
-    else
     end
   end
-  
+
   def has_profile?
     current_user.profile
   end
